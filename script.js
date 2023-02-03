dd7b7008d3373241f1dd728b0697be2a3bc7e2e1
Mahliyo <budur1202@gmail.com>
2023-02-03 18:34:30 +0300

    lesson 2

---
+++ script.js
@@ -0,0 +1,20 @@
+let yosh = prompt("ismingiz");
+let yil = prompt("yilingzni kiriting");
+
+console.log(yosh);
+console.log(yil);
+
+let a = 5;
+let b = 20;
+let c = 40;
+let d = 25;
+let e = 12;
+let f = 10;
+
+let sum = ((a + b) / d) * c;
+
+console.log(sum);
+
+let sum2 = (a + b) ** 3 / (d + c) ** 2 + e * f;
+
+console.log(sum2);
